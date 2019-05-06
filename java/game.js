b15502f5f592e2cd21ab0bdeadf1fe90d04f6912
Aldair <aldairgarcia803@gmail.com>
2019-05-06 14:08:57 +0000

    aiushd

--- java/game.js
+++ java/game.js
@@ -1,28 +1,21 @@
 /*global $*/
+
 $("body").keydown(function(event) {
     //moves left
+    if (event.which === 37 && event.which === 38){
+        $("#blue").css("left", $("#blue").offset().left - 4);
+        $("#blue").css("top", $("#blue").offset().top -4);
+        }
     if (event.which === 37) {
         $("#blue").css("left", $("#blue").offset().left - 4);
     //moves right
-    console.log("1");
     } else if (event.which === 39) {
-        $("#blue").css("left", $("#blue").offset().left + 4);
-        console.log("2");
+        $("#blue").css("left", $("#blue").offset().left + 10);
     } 
     else if (event.which === 38) {
-        $("#blue").css("top", $("#blue").offset().top - 4);
-        console.log("3");
+        $("#blue").css("top", $("#blue").offset().top -4);
     //moves right
     } else if (event.which === 40) {
-        $("#blue").css("top", $("#blue").offset().top + 4);
-        console.log("4");
-    } else {
-        return;
+        $("#blue").css("top", $("#blue").offset().top +4);
     }
-    
-
 });
-
-$("#test").click(function(){
-    console.log("hi");
-})
