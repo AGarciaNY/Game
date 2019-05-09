ce122b5303ae3cacdcf0c45a8eec3ee94278f28a
Aldair <aldairgarcia803@gmail.com>
2019-05-09 19:26:51 +0000

    sadf

--- java/m.js
+++ java/m.js
@@ -1,17 +1,17 @@
 /*global $*/
 var charactercho=[];
+var character=["http://24.media.tumblr.com/tumblr_md1a8v4nzF1qgd7tso1_500.gif",
+               "https://steamuserimages-a.akamaihd.net/ugc/874126486246040200/6D8CAB9283F1FFFBBA91E024B37CAE1C5DDD02EF/",
+               "https://media.giphy.com/media/38NtHuRHQggtq/giphy.gif",
+               "https://66.media.tumblr.com/tumblr_maore5pyvZ1rfjowdo1_500.gif"];
 function chooscharacter(charactercho){
-    var character=["http://24.media.tumblr.com/tumblr_md1a8v4nzF1qgd7tso1_500.gif",
-                   "https://steamuserimages-a.akamaihd.net/ugc/874126486246040200/6D8CAB9283F1FFFBBA91E024B37CAE1C5DDD02EF/",
-                   "https://media.giphy.com/media/38NtHuRHQggtq/giphy.gif",
-                   "https://66.media.tumblr.com/tumblr_maore5pyvZ1rfjowdo1_500.gif"];
     
     for(var i=0;i<character.length;i++){
         $(".characterdiv").append("<img class='character' id='cha"+i+"' src='"+character[i]+"'>");
     }
     $("#cha0").click(function(){
         alert("run");
-        var charactercho=character[0];
+        charactercho.push(character[0]);
         
         document.getElementById("cha1").style.visibility = "hidden";
         document.getElementById("cha2").style.visibility = "hidden";
