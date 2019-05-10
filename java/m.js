f0973f395d88fa4d4d016e503d67cdfe73e97d91
Aldair <aldairgarcia803@gmail.com>
2019-05-10 15:28:29 +0000

    sda

--- java/m.js
+++ java/m.js
@@ -4,48 +4,77 @@ var character=["http://24.media.tumblr.com/tumblr_md1a8v4nzF1qgd7tso1_500.gif",
                "https://steamuserimages-a.akamaihd.net/ugc/874126486246040200/6D8CAB9283F1FFFBBA91E024B37CAE1C5DDD02EF/",
                "https://media.giphy.com/media/38NtHuRHQggtq/giphy.gif",
                "https://66.media.tumblr.com/tumblr_maore5pyvZ1rfjowdo1_500.gif"];
-function chooscharacter(charactercho){
+function chooscharacter(){
     
     for(var i=0;i<character.length;i++){
         $(".characterdiv").append("<img class='character' id='cha"+i+"' src='"+character[i]+"'>");
+        $("cah"+i).css({"position":"absolute"});
     }
     $("#cha0").click(function(){
-        alert("run");
+        
         charactercho.push(character[0]);
         
         document.getElementById("cha1").style.visibility = "hidden";
         document.getElementById("cha2").style.visibility = "hidden";
         document.getElementById("cha3").style.visibility = "hidden";
+        $(document).ready(function(){
+        $("#cha0").animate({
+            position: "absolute",
+            left: '45%',
+            height: '200px',
+            width: '200px'
+            });
+        });
     });
     $("#cha1").click(function(){
-        alert("run");
-        charactercho=character[1];
+        charactercho.push(character[1]);
         
         document.getElementById("cha0").style.visibility = "hidden";
         document.getElementById("cha2").style.visibility = "hidden";
         document.getElementById("cha3").style.visibility = "hidden";
+        $(document).ready(function(){
+        $("#cha1").animate({
+            position: "absolute",
+            left: '45%',
+            height: '200px',
+            width: '200px'
+            });
+        });
         
     });
     $("#cha2").click(function(){
-        alert("run");
-        charactercho=character[2];
+        charactercho.push(character[2]);
         
         document.getElementById("cha1").style.visibility = "hidden";
         document.getElementById("cha0").style.visibility = "hidden";
         document.getElementById("cha3").style.visibility = "hidden";
-        
+        $(document).ready(function(){
+        $("#cha2").animate({
+            position: "absolute",
+            left: '45%',
+            height: '200px',
+            width: '200px'
+            });
+        });
     });
     $("#cha3").click(function(){
-        alert("run");
-        charactercho=character[3];
+        charactercho.push(character[3]);
         
         document.getElementById("cha1").style.visibility = "hidden";
         document.getElementById("cha0").style.visibility = "hidden";
         document.getElementById("cha2").style.visibility = "hidden";
+        $(document).ready(function(){
+        $("#cha3").animate({
+            position: "absolute",
+            left: '45%',
+            height: '200px',
+            width: '200px'
+            });
         
+        });
     });
 }
 chooscharacter();
-$("#cha0").click(function(){
+$("body").click(function(){
   console.log(charactercho);  
 });
