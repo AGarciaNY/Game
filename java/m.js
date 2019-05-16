c6362f113680cd7de1c25e98be0d8a38eb947150
Aldair <aldairgarcia803@gmail.com>
2019-05-16 15:30:05 +0000

    sadasda

--- java/m.js
+++ java/m.js
@@ -1,9 +1,13 @@
 /*global $*/
+var blocks="https://vignette.wikia.nocookie.net/nintendo/images/6/63/Brick_Block.png/revision/latest?cb=20151206062204&path-prefix=en";
+var mistorybox="https://vignette.wikia.nocookie.net/nintendo/images/0/02/Question_Block_NSMB.png/revision/latest?cb=20151206055532&path-prefix=en";
+
 var charactercho=[];
 var character=["http://24.media.tumblr.com/tumblr_md1a8v4nzF1qgd7tso1_500.gif",
                "https://steamuserimages-a.akamaihd.net/ugc/874126486246040200/6D8CAB9283F1FFFBBA91E024B37CAE1C5DDD02EF/",
                "https://media.giphy.com/media/38NtHuRHQggtq/giphy.gif",
                "https://66.media.tumblr.com/tumblr_maore5pyvZ1rfjowdo1_500.gif"];
+var hasgamestart=false;
 function chooscharacter(){
     
     for(var i=0;i<character.length;i++){
@@ -103,6 +107,7 @@ function LoadingPage(){
                         startload.push("three");
                         startload.push("three");
                         $("html").css({"cursor":"context-menu"});
+                        hasgamestart=true;
                     }
                     if(times < n){
                         times=times-1;
@@ -111,21 +116,42 @@ function LoadingPage(){
                     if(times>n){
                         times=times-1; 
                         }
+                    console.log(hasgamestart);
                     console.log(startload.length);
                     
                     }
                     
                 }, 1000);
+                console.log(hasgamestart);
             }
             
-                
+           $(".test").click(function(){
+                console.log(hasgamestart);
+                console.log("hi there");
+                });
+            console.log(hasgamestart);
+            if(hasgamestart===true){
+                console.log("nooooooooo");
+                $("body").html("<body>"+"<h1>hi</h1>"+"<body>");
+                $("body").keydown(function(event) {
+                    if (event.which === 37) {
+                        $("#blue").css("left", $("#blue").offset().left - 4);
+                    //moves right
+                    } else if (event.which === 39) {
+                        $("#blue").css("left", $("#blue").offset().left + 10);
+                    }else if (event.which === 38) {
+                        $("#blue").css("top", $("#blue").offset().top -4);
+                    //moves right
+                    }
+                    
+                });
+            }
         });
-    $(".test").click(function(){
-        console.log(startload);
-        console.log("hi there");
-        });
-    console.log(startload);
-     
+    
+
+            
+      
+
 }
 
 LoadingPage();
